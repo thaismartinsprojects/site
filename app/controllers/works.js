cb6e2f0e6f9c7e5ee3673bfcfb33fcfc001a481f
Thais Martins <contato@thaismartins.rocks>
2015-09-12 02:32:36 -0300

    Page Works

--- app/controllers/works.js
+++ app/controllers/works.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var app = angular.module('thaisMartins');
-app.controller('WorksCtrl', ['$scope',
-                    function ($scope) {
+app.controller('WorksCtrl', ['$scope', 'WorksService',
+                    function ($scope, WorksService) {
+
+    $scope.works = WorksService.getItems();
+    $scope.showWork = false;
 
 }]);
---
+++ app/services/works.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var app = angular.module('thaisMartins');
+app.factory('WorksService', function () {
+
+    var items = [
+        {id: 1, name: 'Viva Design Hostel', description: 'Site institucional', path: 'vivahostel', images: [
+            {id: 1, name: 'Home', image: 'home.jpg'},
+            {id: 2, name: 'Conteúdo Interno', image: 'conteudo.jpg'},
+            {id: 3, name: 'Listagem de Fotos', image: 'fotos.jpg'},
+            {id: 4, name: 'Tour', image: 'tour.jpg'}
+        ], tags: ['design', 'layout', 'html', 'css', 'javascript'], url: 'http://www.vivahostel.com.br/'},
+        {id: 2, name: 'Apperone', description: 'Layout do Aplicativo', path: 'apperone', images: [
+            {id: 1, name: 'Home', image: 'home.jpg'},
+            {id: 2, name: 'Busca', image: 'busca.jpg'},
+            {id: 3, name: 'Listagem de Estabelecimentos', image: 'lista.jpg'},
+            {id: 4, name: 'Conteúdo Interno', image: 'detalhes.jpg'}
+        ], tags: ['design'], url: ''}
+    ];
+
+    return {
+        getItems: function() {
+            return items;
+        },
+        getItem: function(id) {
+            return items[id];
+        }
+    };
+});
