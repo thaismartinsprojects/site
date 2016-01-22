6a5bb6b5521b2ef87d2d84b5df4038beabbc62e7
Thais Martins <contato@thaismartins.rocks>
2016-01-22 11:38:13 -0200

    Change Colors

--- app/filters/splitWords.js
+++ app/filters/splitWords.js
@@ -2,12 +2,15 @@
 
 var app = angular.module('thaisMartins');
 app.filter('splitWords', ['$sce',  function($sce) {
-    return function(input) {
+    return function(input, style) {
 
         var words = input.split(' ');
 
         if(words.length == 2)
-            input = '<strong>' + words[0] + '</strong><br>' + words[1] + '<span class="dot">.</span>';
+            input = '<strong>' + words[0] + '</strong><br>' + words[1];
+
+        if(style == 'dot')
+            input += '<span class="dot"></span>';
 
         console.log(input);
         return $sce.trustAsHtml(input);
