dfdb708921f25eecd80d46c6613fdfb58b217f84
Thais Martins <contato@thaismartins.rocks>
2016-01-24 00:54:47 -0200

    Change skills page

--- app/services/works.js
+++ app/services/works.js
@@ -4,19 +4,25 @@ var app = angular.module('thaisMartins');
 app.factory('WorksService', function () {
 
     var items = [
-        {id: 1, name: 'Viva Design Hostel', shortdescription: 'Criação do site institucional', path: 'vivahostel', images: [
+        {id: 1, name: 'Viva Design Hostel', shortdescription: 'Criação do Site Institucional', path: 'vivahostel',
+        description: 'Cuidei de todo processo criativo da logomarca e do website. Foi responsável pelo desenvolvimento frontend (html/ccs). ',
+        images: [
             {id: 1, name: 'Home', image: 'home.jpg'},
             {id: 2, name: 'Conteúdo Interno', image: 'conteudo.jpg'},
             {id: 3, name: 'Listagem de Fotos', image: 'fotos.jpg'},
             {id: 4, name: 'Tour', image: 'tour.jpg'}
         ], tags: ['design', 'layout', 'html', 'css', 'javascript'], url: 'http://www.vivahostel.com.br/'},
-        {id: 2, name: 'Apperone', shortdescription: 'Layout do Aplicativo', path: 'apperone', images: [
+        {id: 2, name: 'Apperone', shortdescription: 'Design do Aplicativo', path: 'apperone',
+        description: 'Design do Aplicativo',
+        images: [
             {id: 1, name: 'Home', image: 'home.jpg'},
             {id: 2, name: 'Busca', image: 'busca.jpg'},
             {id: 3, name: 'Listagem de Estabelecimentos', image: 'lista.jpg'},
             {id: 4, name: 'Conteúdo Interno', image: 'detalhes.jpg'}
         ], tags: ['design'], url: ''},
-        {id: 3, name: 'Mão na Roda', shortdescription: 'Criação do frontend', path: 'maonaroda', images: [
+        {id: 3, name: 'Mão na Roda', shortdescription: 'Desenvolvimento Frontend', path: 'maonaroda',
+        description: 'Desenvolvimento Frontend',
+        images: [
             {id: 1, name: 'Home', image: 'home.jpg'},
             {id: 2, name: 'Orçamento', image: 'orcamento.jpg'},
         ], tags: ['html', 'css', 'javascript', 'jquery', 'bootstrap'], url: ''}
