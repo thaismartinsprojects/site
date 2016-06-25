29a13a68863160f87825ea8418a1d95dad212a1a
Thais Martins <contato@thaismartins.co>
2016-06-25 13:06:21 -0300

    Adding works API

--- api/models/Work.js
+++ api/models/Work.js
@@ -1,14 +1,17 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
-    utils = require('../services/utils'),
-    ImageSchema = require('./Image').schema;
+    utils = require('../services/utils');
 
 var WorkSchema   = new Schema({
     name: {type: String, required: true, set: generatePath},
     shortdescription: {type: String, required: true},
     path: {type: String, required: true},
     description: {type: String, required: true},
-    images: [ImageSchema],
+    images: [{
+        title: {type: String, required: true},
+        file: {type: String, required: true},
+        created: { type: Date, default: Date.now }
+    }],
     tags: [String],
     url: String,
     created: { type: Date },
