4e1ec20da7754ed08807315dfe0712c96c7b8a7d
Thais Martins <contato@thaismartins.co>
2016-09-29 14:53:50 -0300

    Adding on scroll

--- app/controllers/skills.js
+++ app/controllers/skills.js
@@ -3,24 +3,24 @@
 angular.module('thaisMartins')
 .controller('SkillsController', ['$scope', 'SkillsService', function ($scope, SkillsService) {
 
-    var skills = SkillsService.getAll();
+    $scope.skills = SkillsService.getAll();
     var current = 0;
 
     $scope.showAllSkills = window.isMobile;
-    $scope.skill = skills[current];
-
-    $scope.prevSkill = function() {
-
-        current--;
-        if(current < 0)current = (skills.length - 1);
-        $scope.skill = skills[current];
-    };
-
-    $scope.nextSkill = function() {
-
-        current++;
-        if(current > (skills.length - 1)) current = 0;
-        $scope.skill = skills[current];
-    };
+    // $scope.skill = skills[current];
+    //
+    // $scope.prevSkill = function() {
+    //
+    //     current--;
+    //     if(current < 0)current = (skills.length - 1);
+    //     $scope.skill = skills[current];
+    // };
+    //
+    // $scope.nextSkill = function() {
+    //
+    //     current++;
+    //     if(current > (skills.length - 1)) current = 0;
+    //     $scope.skill = skills[current];
+    // };
 
 }]);
---
+++ app/directives/on-scroll.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var app = angular.module('thaisMartins');
+app.directive('onScroll', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attr) {
+
+            if(typeof attr.onScroll == 'undefined') return false;
+
+            $(window).scroll(function() {
+                var startPosition = $(element).offset().top - ($(window).height() / 2);
+                var position = $(window).scrollTop();
+
+                if(position >= startPosition)
+                    $(element).addClass( attr.onScroll );
+                else
+                    $(element).removeClass( attr.onScroll );
+            });
+        }
+    };
+});
