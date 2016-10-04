675f1a6758a9dcd49a2611690e26c2799839c021
Thais Martins <contato@thaismartins.co>
2016-10-04 16:24:17 -0300

    Adding send

--- app/services/contact.js
+++ app/services/contact.js
@@ -3,7 +3,7 @@
 var app = angular.module('thaisMartins');
 app.factory('ContactService', ['$http', 'RESOURCES', function ($http, RESOURCES) {
 
-    var apiUrl = RESOURCES.API_URL + '/accounts';
+    var apiUrl = RESOURCES.API_URL + '/contact';
     $http.defaults.headers.post["Content-Type"] = "application/json";
 
     return {
